a017cbf07acf5040416b76ffcb1c47d2e4590ebb
Alex Chebotarsky <goodleby@gmail.com>
2021-11-27 23:04:18 +0200

    feat: 0.0.8, basic functionality, but getting Minified React error 321

--- src/index.ts
+++ src/index.ts
@@ -10,26 +10,25 @@ export const createTheming = <T extends { [color: string]: string }, N>(
     };
   }
 ): readonly [
-  React.Provider<'main' | keyof N | null>,
-  (themeName?: keyof N | undefined) => T,
-  {
-    main: T;
-  } & { [themeName in keyof N]: { [color in keyof T]?: string | undefined } }
+  React.Provider<typeof defaultName | keyof N | null>,
+  (themeName?: typeof defaultName | keyof N) => T,
+  { [defaultName]: T } & typeof customThemes
 ] => {
   const themes = { [defaultName]: mainTheme, ...customThemes };
 
-  const ThemeContext = React.createContext<keyof N | typeof defaultName | null>(
-    null
-  );
+  const ThemeContext = React.createContext<keyof typeof themes | null>(null);
 
-  const useTheme = (themeName?: keyof N) => {
+  const useTheme = (themeName?: keyof typeof themes) => {
     const contextName = useContext(ThemeContext);
+
     if (!themeName && contextName === null) {
       console.warn(
-        '[react-color-theme]: It looks like you are using the useTheme hook in the same file that you use to render the theme provider. At the time the hook is called, the theme is set to `null`'
+        '[react-color-theme]: Theme name is not provided, it happens if the theme provider is not rendered at the time useTheme hook is called. It might happen if useTheme is called in the same file where the provider is rendered, in that case it is recommended to refactor code to avoid that. Or if it is not possible you can pass the theme name to useTheme hook as a parameter.'
       );
     }
+
     const name = themeName || contextName || defaultName;
+
     return name === defaultName
       ? themes[defaultName]
       : { ...themes[defaultName], ...themes[name] };
